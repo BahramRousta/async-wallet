2128d0c4077ed7569422998b7d760feb5a1d210d
bahramrousta <rousta.bahram@gmail.com>
2024-02-11 19:46:30 +0330

    add balance api

--- infrastructure/repository.py
+++ infrastructure/repository.py
@@ -1,6 +1,6 @@
-from domain.events import WalletCreated
+from domain.events import WalletCreated, Deposited, Withdrawn
 from domain.models import Wallet
-from infrastructure.db import mongo_instance
+from infrastructure.data_access import mongo_instance
 
 
 class WalletCommandRepository:
@@ -12,11 +12,46 @@ class WalletCommandRepository:
         await self.db.wallet_collection.insert_one(wallet.model_dump())
         await self.db.event_collection.insert_one(event.model_dump())
 
+    async def deposit(self, event: Deposited) -> None:
+        await self.db.wallet_collection.find_one_and_update(
+            {'wallet_id': event.wallet_id},
+            {'$inc': {'balance': event.amount}}
+        )
+
+        await self.db.event_collection.insert_one(event.model_dump())
+
+    async def withdraw(self, event: Withdrawn) -> None:
+        wallet = await self.db.wallet_collection.find_one(
+            {'wallet_id': event.wallet_id}
+        )
+
+        balance = wallet['balance']
+        if balance <= 0:
+            raise ValueError('Unable to withdraw')
+
+        await self.db.wallet_collection.update_one(
+            {'wallet_id': event.wallet_id},
+            {'$inc': {'balance': -event.amount}}
+        )
+        await self.db.event_collection.insert_one(event.model_dump())
+
 
 class WalletQueryRepository:
     def __init__(self):
         self.db = mongo_instance
 
-    async def get_wallet(self, user_id: int) -> dict | None:
-        wallet = await self.db.wallet_collection.find_one({'user_id': user_id})
+    async def get_wallet(self, user_id: int = None, wallet_id: str = None) -> dict | None:
+
+        if not wallet_id:
+            wallet = await self.db.wallet_collection.find_one({'user_id': user_id})
+        else:
+            wallet = await self.db.wallet_collection.find_one({'wallet_id': wallet_id})
         return wallet if wallet else None
+
+    async def get_balance(self, wallet_id: str) -> float:
+        wallet = await self.get_wallet(wallet_id=wallet_id)
+
+        if not wallet:
+            raise ValueError("Could not find wallet")
+        return wallet['balance']
+
--- presentation/main.py
+++ presentation/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, status, HTTPException
-from domain.events import WalletCreated
-from presentation.schemas import GetWalletOutSchema
-from services.commands import CreateWalletCommand
-from services.queries import WalletQueryService
+from domain.events import WalletCreated, Deposited, Withdrawn
+from presentation.schemas import GetWalletOutSchema, DepositIn, WithdrawIn
+from services.commands import CreateWalletCommand, DepositCommand, WithdrawCommand
+from services.queries import WalletQueryService, WalletBalanceQueryService
 
 app = FastAPI()
 
@@ -39,3 +39,66 @@ async def get_wallet(user_id: int) -> dict:
         )
 
     return existing_wallet
+
+
+@app.post('/deposit/')
+async def deposit(deposit: DepositIn) -> dict:
+
+    wallet_id = deposit.wallet_id
+    existing_wallet = await WalletQueryService().execute(wallet_id=wallet_id)
+
+    if not existing_wallet:
+        raise HTTPException(
+            status_code=404,
+            detail='Wallet does not exist.'
+        )
+
+    event = Deposited(wallet_id=wallet_id, amount=deposit.amount)
+    await DepositCommand().execute(event=event)
+
+    return {
+        'data': event.model_dump(),
+        'message': "Wallet created successfully",
+        'status': status.HTTP_200_OK
+    }
+
+
+@app.post('/withdraw/')
+async def withdraw(withdraw: WithdrawIn) -> dict:
+    wallet_id = withdraw.wallet_id
+    existing_wallet = await WalletQueryService().execute(wallet_id=wallet_id)
+
+    if not existing_wallet:
+        raise HTTPException(
+            status_code=404,
+            detail='Wallet does not exist.'
+        )
+
+    event = Withdrawn(wallet_id=wallet_id, amount=withdraw.amount)
+    try:
+        await WithdrawCommand().execute(event=event)
+
+        return {
+            'data': event.model_dump(),
+            'message': "Wallet created successfully",
+            'status': status.HTTP_200_OK
+        }
+    except Exception as e:
+        return {
+            'data': e.args,
+            'message': "Withdraw failed.",
+            'status': status.HTTP_400_BAD_REQUEST
+        }
+
+
+@app.get('/balance/{wallet_id}/')
+async def wallet_balance(wallet_id: str) -> dict:
+    try:
+        balance = await WalletBalanceQueryService().execute(wallet_id=wallet_id)
+        return {'balance': balance}
+    except Exception as e:
+        return {
+            'data': e.args,
+            'message': "Withdraw failed.",
+            'status': status.HTTP_400_BAD_REQUEST
+        }
