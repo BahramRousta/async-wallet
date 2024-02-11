dc1cbcb323812c7df9895efb757be0721e2342b8
bahramrousta <rousta.bahram@gmail.com>
2024-02-11 22:07:39 +0330

    some improve in data access class

--- infrastructure/data_access.py
+++ infrastructure/data_access.py
@@ -1,11 +1,20 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import ConnectionFailure
+from domain.exceptions import MongoConnectionError
 
 
 class MongoManager:
 
-    def __init__(self):
+    def __init__(self, max_pool_size=100):
         self.mongo_url = 'mongodb://localhost:27017'
-        self.client = AsyncIOMotorClient(self.mongo_url)
+        self.max_pool_size = max_pool_size
+
+        try:
+            self.client = AsyncIOMotorClient(self.mongo_url, maxPoolSize=self.max_pool_size)
+            self.database = self.client.get_database('Wallet')
+        except ConnectionFailure as e:
+            raise MongoConnectionError(f"Failed to connect to MongoDB: {e}")
+
         self.database = self.client['Wallet']
 
     @property
